46a08e7fc6d91e12c3fdcff6b44307e230cec29a
yohei.yamana <y.yamana@opt.ne.jp>
2016-12-16 15:00:10 +0900

    training

--- build.sbt
+++ build.sbt
@@ -11,6 +11,20 @@ resolvers ++= Seq(
 libraryDependencies ++= Seq(
   "org.eclipse.jgit" % "org.eclipse.jgit" % "4.5.0.201609210915-r",
   "org.eclipse.jgit" % "org.eclipse.jgit.archive" % "4.5.0.201609210915-r",
-  "org.eclipse.mylyn.github" % "org.eclipse.egit.github.core" % "2.1.5"
+  "org.eclipse.mylyn.github" % "org.eclipse.egit.github.core" % "2.1.5",
+  "org.slf4j" % "slf4j-simple" % "1.7.22"
 )
+
+assemblyMergeStrategy in assembly := {
+  case PathList("javax", "servlet", xs @ _*)         => MergeStrategy.first
+  case PathList(ps @ _*) if ps.last endsWith ".properties" => MergeStrategy.first
+  case PathList(ps @ _*) if ps.last endsWith ".xml" => MergeStrategy.first
+  case PathList(ps @ _*) if ps.last endsWith ".types" => MergeStrategy.first
+  case PathList(ps @ _*) if ps.last endsWith ".class" => MergeStrategy.first
+  case "application.conf"                            => MergeStrategy.concat
+  case "unwanted.txt"                                => MergeStrategy.discard
+  case x =>
+    val oldStrategy = (assemblyMergeStrategy in assembly).value
+    oldStrategy(x)
+}
 
---
+++ src/main/scala/EntryPoint.scala
@@ -0,0 +1,34 @@
+
+object EntryPoint {
+  import scala.util.Try
+  import java.io.{File, IOException}
+  import org.eclipse.jgit.api.Git
+  import org.eclipse.jgit.api.errors.GitAPIException
+  import scala.collection.JavaConverters._
+
+  def main(args: Array[String]) = 
+    args.headOption match {
+      case Some(url) => 
+        val localPath = File.createTempFile("TestGitRepository", "")
+        if(!localPath.delete()) {
+            throw new IOException(s"Could not delete temporary file $localPath")
+        }
+
+        // then clone
+        println(s"Cloning from $url to $localPath")
+        Try {
+          Git.cloneRepository()
+                .setURI(url)
+                .setDirectory(localPath)
+                .call()
+        }.foreach{ git =>
+        // Note: the call() returns an opened repository already which needs to be closed to avoid file handle leaks!
+          println(s"Having repository: ${git.getRepository().getDirectory()}")
+
+          git.log()//.addPath("--merges")
+            .call().asScala.foreach{ l => println(l.getShortMessage()) } 
+        }
+      case None => println("No repository specified.")
+    }
+
+}
