1fc4e5edb158e5bcfb5326672fe5f912485247df
yohei.yamana <y.yamana@opt.ne.jp>
2016-12-16 19:15:47 +0900

    refactor

--- src/main/scala/EntryPoint.scala
+++ src/main/scala/EntryPoint.scala
@@ -1,14 +1,17 @@
 
 object EntryPoint {
   import scala.util.Try
+  import scala.collection.JavaConverters._
+  import scala.io.Source._
   import java.io.{File, IOException}
   import org.eclipse.jgit.api.Git
   import org.eclipse.jgit.api.errors.GitAPIException
   import org.eclipse.jgit.revwalk.filter.RevFilter
-  import scala.collection.JavaConverters._
+  import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
 
   def main(args: Array[String]) = 
     args.headOption match {
+      case None => println("No repository specified.")
       case Some(url) => 
         val localPath = File.createTempFile("TestGitRepository", "")
         if(!localPath.delete()) {
@@ -17,19 +20,35 @@ object EntryPoint {
 
         // then clone
         println(s"Cloning from $url to $localPath")
-        Try {
-          Git.cloneRepository()
-                .setURI(url)
-                .setDirectory(localPath)
-                .call()
-        }.foreach{ git =>
-        // Note: the call() returns an opened repository already which needs to be closed to avoid file handle leaks!
-          println(s"Having repository: ${git.getRepository().getDirectory()}")
 
-          git.log().setRevFilter(RevFilter.ONLY_MERGES)//.addPath("--merges")
-            .call().asScala.foreach{ l => println(l.getShortMessage()) } 
+        val credential = Try{ fromFile("GH_CREDENTIAL") }.toOption
+          .flatMap(_.getLines.toStream.headOption)
+          .flatMap(
+            _.split(",").toSeq match {
+              case Seq(u, p) => Some((u, p))
+              case _ => None
+            }
+          )
+
+        val cloneCommand = Git.cloneRepository()
+                              .setURI(url)
+                              .setDirectory(localPath)
+        credential.foreach {
+          case (u, p) => 
+            cloneCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider(u, p)) 
         }
-      case None => println("No repository specified.")
+
+        Try { cloneCommand.call() }.toOption match {
+          case Some(git) =>
+            println(s"Having repository: ${git.getRepository().getDirectory()}")
+            git
+              .log()
+              .setRevFilter(RevFilter.ONLY_MERGES)
+              .call()
+              .asScala
+              .foreach{ l => println(l.getShortMessage()) } 
+          case _ => println("clone failed.")
+        }
     }
 
 }
